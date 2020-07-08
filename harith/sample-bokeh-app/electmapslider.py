7e4f11a9704314d2a24697af632dac25365d329f
srinerella <34721852+srinerella@users.noreply.github.com>
2020-07-07 23:02:52 -0400

    updating for F2020

--- harith/sample-bokeh-app/electmapslider.py
+++ harith/sample-bokeh-app/electmapslider.py
@@ -83,11 +83,12 @@ def init_data():
     #total_data = "https://raw.githubusercontent.com/ehuang13/w210-presidential-election/master/data/combined_jun22.csv"
     #combined_df = pd.read_csv(total_data, encoding = "ISO-8859-1")
 
-    combined_df = pd.read_csv("https://raw.githubusercontent.com/ehuang13/w210-presidential-election/master/data/Data-Jul07/combined_jul05.csv",encoding = "ISO-8859-1")
+    combined_df = pd.read_csv("https://raw.githubusercontent.com/ehuang13/w210-presidential-election/master/data/Data-Jul07/Input_Output_Jul07.csv",encoding = "ISO-8859-1")
 
     #winning_party_binary = pd.get_dummies(combined_df["WINNING_PARTY"], drop_first = True)
     #combined_df["WINNING_PARTY_BINARY"] = winning_party_binary
     combined_df['FIPS']=combined_df['STATE_FIPS']*1000 + combined_df['COUNTY_FIPS']
+    combined_df = combined_df.loc[~combined_df["STATE_FIPS"].isin( [2, 15])]
     # read in counties shapefile from US Census Bureau
     counties_usa = gpd.read_file("bokeh/cb_2018_us_county_20m.shp")
     print("Counties Shapefile Dimensions: {}".format(counties_usa.shape))
@@ -113,7 +114,7 @@ def get_electmap_with_controls():
 
     merged_cnt_data = init_data()
 
-    year_select = Slider(start = 2000, end = 2016,
+    year_select = Slider(start = 2000, end = 2020,
                          step = 4, value = 2000,
                          title = 'Election Year')
 
